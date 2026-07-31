9739cc8e77af9ca162b6c7110dfd90a291aa3d42
BaldGrid <misswangmi229028@outlook.com>
2026-07-31 21:14:31 +0800

    第一次更新上传喵

---
+++ script.js
@@ -0,0 +1,55 @@
+let list=document.getElementById("list");
+let search=document.getElementById("search");
+
+let data=[];
+
+fetch("data/resources.json")
+.then(r=>r.json())
+.then(d=>{
+data=d;
+show(data);
+});
+
+
+function show(arr){
+
+list.innerHTML="";
+
+arr.forEach(x=>{
+
+list.innerHTML+=`
+
+<div class="card">
+
+<h2>${x.name}</h2>
+
+<p>${x.type}</p>
+
+<p>${x.desc}</p>
+
+<a href="${x.url}">
+下载 / 查看
+</a>
+
+</div>
+
+`;
+
+});
+
+}
+
+
+search.oninput=function(){
+
+let k=this.value;
+
+show(
+data.filter(x=>
+x.name.includes(k)
+||
+x.type.includes(k)
+)
+);
+
+}
